3ab1e2d1885d452dae1e6ee5f23189c68146542b
Ryo-Kob <k1710243@gmail.com>
2021-12-19 21:55:16 +0900

    Activityを今までと同じやり方で追加してみようとしたけど、うまくいかないね。今回は追加対象がFragmentだからね。

--- app/src/main/java/com/rkb/travelcards/MainActivity.kt
+++ app/src/main/java/com/rkb/travelcards/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.rkb.travelcards
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val newCardActivityRequestCode = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +28,27 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fragment_card_fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewCardActivity::class.java)
+            startActivityForResult(intent, newCardActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newCardActivityRequestCode && resultCode == Activity.RESULT_OK) {
+//            data?.getStringExtra(NewCardActivity.EXTRA_REPLY)?.let {
+//                val word = Word(it)
+//                wordViewModel.insert(word)
+//            }
+        } else {
+//            Toast.makeText(
+//                applicationContext,
+//                R.string.empty_not_saved,
+//                Toast.LENGTH_LONG).show()
+        }
     }
 }
