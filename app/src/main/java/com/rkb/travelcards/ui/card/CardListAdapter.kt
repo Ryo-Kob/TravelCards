4a517568c1d3209db056285e41b1508bb5e2d909
Ryo-Kob <k1710243@gmail.com>
2021-12-21 22:32:41 +0900

    なんとか、なってる、っぽい。

--- app/src/main/java/com/rkb/travelcards/ui/card/CardListAdapter.kt
+++ app/src/main/java/com/rkb/travelcards/ui/card/CardListAdapter.kt
@@ -1,13 +1,18 @@
-package com.rkb.travelcards
+package com.rkb.travelcards.ui.card
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.rkb.travelcards.Card
+import com.rkb.travelcards.R
 
 
-class CardListAdapter : RecyclerView.Adapter<CardListAdapter.ViewHolder>() {
+//class CardListAdapter : RecyclerView.Adapter<CardListAdapter.ViewHolder>() {
+class CardListAdapter : ListAdapter<Card, CardListAdapter.ViewHolder>(CardsComparator()) {
 
     /**
      * Provide a reference to the type of views that you are using
@@ -19,6 +24,10 @@ class CardListAdapter : RecyclerView.Adapter<CardListAdapter.ViewHolder>() {
         init {
             // Define click listener for the ViewHolder's View.
             textView = view.findViewById(R.id.card_text_view_name)
+        }
+
+        fun bind(text: String?) {
+            textView.text = text
         }
     }
 
@@ -37,11 +46,22 @@ class CardListAdapter : RecyclerView.Adapter<CardListAdapter.ViewHolder>() {
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
 
-//        viewHolder.textView.text = dataSet[position]
+//        viewHolder.textView.text = "てきすと"//dataSet[position]
+        val current = getItem(position)
+        viewHolder.bind(current.title) // ここにも注意だ!
     }
 
     // Return the size of your dataset (invoked by the layout manager)
 //    override fun getItemCount() = dataSet.size
-    override fun getItemCount() = 10
+//    override fun getItemCount() = 10
 
+    class CardsComparator : DiffUtil.ItemCallback<Card>() {
+        override fun areItemsTheSame(oldItem: Card, newItem: Card): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Card, newItem: Card): Boolean {
+            return oldItem.title == newItem.title // ここに要注意!
+        }
+    }
 }
