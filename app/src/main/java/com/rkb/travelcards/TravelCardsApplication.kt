c87d9b3f661f9cd9e7a43540dee96e8bf7df4cd8
Ryo-Kob <k1710243@gmail.com>
2022-01-12 11:35:23 +0900

    DarkMode強制解除設定

--- app/src/main/java/com/rkb/travelcards/TravelCardsApplication.kt
+++ app/src/main/java/com/rkb/travelcards/TravelCardsApplication.kt
@@ -3,9 +3,17 @@ package com.rkb.travelcards
 import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class TravelCardsApplication  : Application() {
     val applicationScope = CoroutineScope(SupervisorJob())
+
+    override fun onCreate() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // アプリ全体に適用
+
+        super.onCreate()
+    }
 
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
