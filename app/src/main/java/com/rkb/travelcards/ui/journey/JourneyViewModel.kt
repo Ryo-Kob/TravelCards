7537de4bec69f35b8758b900322d131ab4e9de18
Ryo-Kob <k1710243@gmail.com>
2022-01-19 22:08:49 +0900

    planのviewmodelを作ったところ、repositoryにも変更が必要なことがわかった

--- app/src/main/java/com/rkb/travelcards/ui/journey/JourneyViewModel.kt
+++ app/src/main/java/com/rkb/travelcards/ui/journey/JourneyViewModel.kt
@@ -1,13 +1,32 @@
-package com.rkb.travelcards.ui.journey
+package com.rkb.travelCards.ui.Plan
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.rkb.travelcards.Plan
+import com.rkb.travelcards.TravelCardsRepository
+import kotlinx.coroutines.launch
 
-class JourneyViewModel : ViewModel() {
+class JourneyViewModel(private val repository: TravelCardsRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is journey Fragment"
+    // Using LiveData and caching what allWords returns has several benefits:
+    // - We can put an observer on the data (instead of polling for changes) and only update the
+    //   the UI when the data actually changes.
+    // - Repository is completely separated from the UI through the ViewModel.
+    val allPlans: LiveData<List<Plan>> = repository.allPlans.asLiveData()
+
+    /**
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(Plan: Plan) = viewModelScope.launch {
+        repository.insert(Plan)
     }
-    val text: LiveData<String> = _text
+}
+
+class JourneyViewModelFactory(private val repository: TravelCardsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(JourneyViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return JourneyViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
