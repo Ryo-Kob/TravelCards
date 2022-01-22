b7e9d423a8512e2c5db58c8f2d65b36858e5db9a
Ryo-Kob <k1710243@gmail.com>
2022-01-22 23:06:42 +0900

    onMove, onSqipedの実装に成功した（サンプルほぼそのまま）

--- app/src/main/java/com/rkb/travelcards/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/rkb/travelcards/ui/plan/PlanFragment.kt
@@ -1,6 +1,5 @@
 package com.rkb.travelcards.ui.plan
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
@@ -10,6 +9,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
@@ -19,8 +19,7 @@ import com.rkb.travelcards.*
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 
-class PlanFragment : Fragment()
-    , PlanRecyclerItemClickListener.OnRecyclerClickListener {
+class PlanFragment : Fragment() {
 
     lateinit var cards : List<Card>
 
@@ -63,17 +62,71 @@ class PlanFragment : Fragment()
             cardSuites?.let { adapter.submitList(it) }
         })
 
-//        this.fab = view.findViewById<FloatingActionButton>(R.id.fragment_plan_fab)
-//        this.fab.setOnClickListener {
-//            val intent = Intent(activity, NewplanActivity::class.java)
-//            startActivityForResult(intent, newplanActivityRequestCode)
-//        }
+        // クリックイベント等
+        val mIth = ItemTouchHelper(
+            object : ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+                ItemTouchHelper.LEFT
+            ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    val fromPos = viewHolder.adapterPosition
+                    val toPos = target.adapterPosition
+                    adapter.notifyItemMoved(fromPos, toPos)
 
-//        Log.v("", activity.toString())
-//        Log.v("", activity!!.supportFragmentManager.toString())
-//        Log.v("", activity!!.supportFragmentManager.findFragmentById(R.id.my_nav_host).toString())
-//        Log.v("", activity!!.supportFragmentManager.fragments.toString())
-//        Log.v("", activity!!.supportFragmentManager.fragments[0].toString())
+                    Toast.makeText(
+                        activity,
+                        "Click from:$fromPos, to:$toPos",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    return true // true if moved, false otherwise
+                }
+
+                override fun onSwiped(
+                    viewHolder: RecyclerView.ViewHolder,
+                    direction: Int
+                ) {
+                }
+            })
+        mIth.attachToRecyclerView(recyclerView)
+//        val itemTouchHelper = ItemTouchHelper(
+//            object : ItemTouchHelper.SimpleCallback(
+//                ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.LEFT
+//            ) {
+//                override fun onItemClick(holder: PlanAdapter.ViewHolder) {
+//                    //
+//                    // ここにクリックイベント時の処理を記述
+//                    //
+//                    // 例：Toastを表示
+//                    val _position = holder.adapterPosition  // アイテムのポジションを取得
+//                    val _mesg = holder.textViewName.text    // mesgはエレメントのView(TextView)
+//                    Toast.makeText(
+//                        activity,
+//                        "Click Pos=${_position} Mesg=\"${_mesg}\"",
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//                }
+//
+//                override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
+//
+//                    val fromPosition = viewHolder?.adapterPosition ?: 0
+//                    val toPosition = target?.adapterPosition ?: 0
+//
+//                    recyclerView!!.adapter!!.notifyItemMoved(fromPosition, toPosition)
+//
+//                    return true
+//                }
+//
+//                override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int) {
+//                    viewHolder?.let {
+//                        recyclerView.adapter!!.notifyItemRemoved(viewHolder.adapterPosition)
+//                    }
+//                }
+//            })
+//        itemTouchHelper.attachToRecyclerView(recyclerView)
 
         // ドロワー
         val navHostFragment = activity!!.supportFragmentManager.fragments[0] as NavHostFragment
@@ -81,35 +134,5 @@ class PlanFragment : Fragment()
         val navController = navHostFragment.navController
         view.findViewById<NavigationView>(R.id.my_nav_view).setupWithNavController(navController)
 
-        adapter.itemClickListener = object : PlanAdapter.OnItemClickListener {
-            override fun onItemClick(holder: PlanAdapter.ViewHolder) {
-                //
-                // ここにクリックイベント時の処理を記述
-                //
-                // 例：Toastを表示
-                val _position = holder.adapterPosition  // アイテムのポジションを取得
-                val _mesg = holder.textViewName.text    // mesgはエレメントのView(TextView)
-                Toast.makeText(
-                    activity,
-                    "Click Pos=${_position} Mesg=\"${_mesg}\"",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-    }
-
-    override fun onItemClick(view: View, position: Int) {
-        Toast.makeText(context, "普通のタップ", Toast.LENGTH_SHORT).show()
-        Log.d("MainActivty", "普通のタップ")
-    }
-
-    override fun onItemLongClick(view: View, position: Int) {
-        Toast.makeText(context, "長押しタップ", Toast.LENGTH_SHORT).show()
-        Log.d("MainActivty", "長押しのタップ")
-    }
-
-    override fun onDoubleClick(view: View, position: Int) {
-        Toast.makeText(context, "ダブルタップ", Toast.LENGTH_SHORT).show()
-        Log.d("MainActivty", "ダブルタップ")
     }
 }
