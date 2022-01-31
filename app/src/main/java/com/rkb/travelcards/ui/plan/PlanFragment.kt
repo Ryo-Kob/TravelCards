47709715a406fb1a2537f200e8f441113eec2660
Ryo-Kob <k1710243@gmail.com>
2022-02-01 01:42:32 +0900

    スクロール終わり次第再描画したい問題は、DefaultItemAnimatorのonAnimationFinishedの中でadapter.notifyItemChanged(index)を呼ぶようにしてもだめ。おそらくこの関数が呼ばれすぎて狂う。

--- app/src/main/java/com/rkb/travelcards/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/rkb/travelcards/ui/plan/PlanFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -197,7 +198,14 @@ class PlanFragment : Fragment() {
         mIth.attachToRecyclerView(recyclerView)
 
         // アニメが終わったらイベント
-//        recyclerView.itemAnimator.
+//        recyclerView.itemAnimator = object : DefaultItemAnimator() {
+//            override fun onAnimationFinished(viewHolder: ViewHolder) {
+//                super.onAnimationFinished(viewHolder)
+//
+//                val index = viewHolder.layoutPosition
+//                adapter.notifyItemChanged(index)
+//            }
+//        }
 
         // ドロワー
         val navHostFragment = activity!!.supportFragmentManager.fragments[0] as NavHostFragment
@@ -337,33 +345,6 @@ class PlanFragment : Fragment() {
         for(i in 0..4*24-1) {
             val ntl = i*15
             tl.add(ntl)
-        }
-    }
-
-    inner class CustomScrollListener : OnScrollListener() {
-        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-            when (newState) {
-                SCROLL_STATE_IDLE -> println("The RecyclerView is not scrolling")
-                SCROLL_STATE_DRAGGING -> println("Scrolling now")
-                SCROLL_STATE_SETTLING -> println("Scroll Settling")
-            }
-        }
-
-        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-            if (dx > 0) {
-                println("Scrolled Right")
-            } else if (dx < 0) {
-                println("Scrolled Left")
-            } else {
-                println("No Horizontal Scrolled")
-            }
-            if (dy > 0) {
-                println("Scrolled Downwards")
-            } else if (dy < 0) {
-                println("Scrolled Upwards")
-            } else {
-                println("No Vertical Scrolled")
-            }
         }
     }
 }
