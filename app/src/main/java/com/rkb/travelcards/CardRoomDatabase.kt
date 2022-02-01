b6aa8ebb865f5dd0eb382410649f9c82f586f3c7
Ryo-Kob <k1710243@gmail.com>
2022-02-01 23:14:07 +0900

    初期配置カード変更

--- app/src/main/java/com/rkb/travelcards/CardRoomDatabase.kt
+++ app/src/main/java/com/rkb/travelcards/CardRoomDatabase.kt
@@ -64,52 +64,41 @@ public abstract class CardRoomDatabase : RoomDatabase() {
 
             // Add sample words.
             var card = Card()
-//            card.title = "(例) ××レストランでランチ"
-//            card.description = ""
-//            card.strStartDate = ""
-//            card.strStartTime = ""
-//            card.isStartDateSet = false
-//            card.isStartTimeSet =false
-//            card.strDateTime = "1 時間 0 分"
-//            card.timerHour = 1
-//            card.timerMinute = 0
-//            cardDao.insert(card)
-
-            // さらにサンプルワード。デモ用
-            card.title = "東京 → 京都 新幹線 <予約済み>"
-            card.description = ""
-            card.strStartDate = ""
-            card.strStartTime = "07:00"
-            card.isStartDateSet = false
-            card.isStartTimeSet = true
-            card.strDateTime = "7:00 から 2 時間 30 分"
-            card.timerHour = 2
-            card.timerMinute = 30
-            cardDao.insert(card)
-
-            card.title = "京都 → 奈良 （JR）"
+            card.title = "(例) ××レストランでランチ"
             card.description = ""
             card.strStartDate = ""
             card.strStartTime = ""
             card.isStartDateSet = false
-            card.isStartTimeSet = false
-            card.strDateTime = "1 時間 30 分"
-            card.timerHour = 1
-            card.timerMinute = 30
-            cardDao.insert(card)
-
-            card.title = "京都 → 奈良 （近鉄）"
-            card.description = ""
-            card.strStartDate = ""
-            card.strStartTime = ""
-            card.isStartDateSet = false
-            card.isStartTimeSet = false
+            card.isStartTimeSet =false
             card.strDateTime = "1 時間 0 分"
             card.timerHour = 1
             card.timerMinute = 0
             cardDao.insert(card)
 
-//            card.title = "細かい移動時間"
+            // さらにサンプルワード。デモ用
+//            card.title = "東京 → 京都 新幹線 <予約済み>"
+//            card.description = ""
+//            card.strStartDate = ""
+//            card.strStartTime = "07:00"
+//            card.isStartDateSet = false
+//            card.isStartTimeSet = true
+//            card.strDateTime = "7:00 から 2 時間 30 分"
+//            card.timerHour = 2
+//            card.timerMinute = 30
+//            cardDao.insert(card)
+//
+//            card.title = "京都 → 奈良 （JR）"
+//            card.description = ""
+//            card.strStartDate = ""
+//            card.strStartTime = ""
+//            card.isStartDateSet = false
+//            card.isStartTimeSet = false
+//            card.strDateTime = "1 時間 30 分"
+//            card.timerHour = 1
+//            card.timerMinute = 30
+//            cardDao.insert(card)
+//
+//            card.title = "京都 → 奈良 （近鉄）"
 //            card.description = ""
 //            card.strStartDate = ""
 //            card.strStartTime = ""
@@ -119,50 +108,61 @@ public abstract class CardRoomDatabase : RoomDatabase() {
 //            card.timerHour = 1
 //            card.timerMinute = 0
 //            cardDao.insert(card)
-
-            card.title = "ランチ in 京都"
-            card.description = ""
-            card.strStartDate = ""
-            card.strStartTime = ""
-            card.isStartDateSet = false
-            card.isStartTimeSet = false
-            card.strDateTime = "0 時間 45 分"
-            card.timerHour = 0
-            card.timerMinute = 45
-            cardDao.insert(card)
-
-            card.title = "お寺 見学"
-            card.description = ""
-            card.strStartDate = ""
-            card.strStartTime = ""
-            card.isStartDateSet = false
-            card.isStartTimeSet = false
-            card.strDateTime = "2 時間 0 分"
-            card.timerHour = 2
-            card.timerMinute = 0
-            cardDao.insert(card)
-
-            card.title = "カフェで休憩"
-            card.description = ""
-            card.strStartDate = ""
-            card.strStartTime = ""
-            card.isStartDateSet = false
-            card.isStartTimeSet = false
-            card.strDateTime = "1 時間 30 分"
-            card.timerHour = 1
-            card.timerMinute = 30
-            cardDao.insert(card)
-
-            card.title = "ホテルチェックイン in 奈良xxホテル"
-            card.description = ""
-            card.strStartDate = ""
-            card.strStartTime = "17:00"
-            card.isStartDateSet = false
-            card.isStartTimeSet = true
-            card.strDateTime = "17:00 から 3 時間 0 分"
-            card.timerHour = 3
-            card.timerMinute = 0
-            cardDao.insert(card)
+//
+////            card.title = "細かい移動時間"
+////            card.description = ""
+////            card.strStartDate = ""
+////            card.strStartTime = ""
+////            card.isStartDateSet = false
+////            card.isStartTimeSet = false
+////            card.strDateTime = "1 時間 0 分"
+////            card.timerHour = 1
+////            card.timerMinute = 0
+////            cardDao.insert(card)
+//
+//            card.title = "ランチ in 京都"
+//            card.description = ""
+//            card.strStartDate = ""
+//            card.strStartTime = ""
+//            card.isStartDateSet = false
+//            card.isStartTimeSet = false
+//            card.strDateTime = "0 時間 45 分"
+//            card.timerHour = 0
+//            card.timerMinute = 45
+//            cardDao.insert(card)
+//
+//            card.title = "お寺 見学"
+//            card.description = ""
+//            card.strStartDate = ""
+//            card.strStartTime = ""
+//            card.isStartDateSet = false
+//            card.isStartTimeSet = false
+//            card.strDateTime = "2 時間 0 分"
+//            card.timerHour = 2
+//            card.timerMinute = 0
+//            cardDao.insert(card)
+//
+//            card.title = "カフェで休憩"
+//            card.description = ""
+//            card.strStartDate = ""
+//            card.strStartTime = ""
+//            card.isStartDateSet = false
+//            card.isStartTimeSet = false
+//            card.strDateTime = "1 時間 30 分"
+//            card.timerHour = 1
+//            card.timerMinute = 30
+//            cardDao.insert(card)
+//
+//            card.title = "ホテルチェックイン in 奈良xxホテル"
+//            card.description = ""
+//            card.strStartDate = ""
+//            card.strStartTime = "17:00"
+//            card.isStartDateSet = false
+//            card.isStartTimeSet = true
+//            card.strDateTime = "17:00 から 3 時間 0 分"
+//            card.timerHour = 3
+//            card.timerMinute = 0
+//            cardDao.insert(card)
         }
 
         suspend fun populateDatabase(cardSuiteDao: CardSuiteDao) {
